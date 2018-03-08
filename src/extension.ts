b75b22fa9f5e3a3ec77375ef17ddc566ae446c3d
Dale Newman <dalenewman@gmail.com>
2018-03-07 22:33:26 -0500

    messing around with snippets

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Client } from '_debugger';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,24 +17,31 @@ export function activate(context: vscode.ExtensionContext) {
     // The commandId parameter must match the command field in package.json
 
     let tflRun = vscode.commands.registerCommand('tfl.run', () => {
-        // The code you place here will be executed every time your command is executed
         run("default", null);
     });
 
     let tflSchema = vscode.commands.registerCommand('tfl.schema', () => {
-        // The code you place here will be executed every time your command is executed
         run("check", null);
     });
 
     let tflInit = vscode.commands.registerCommand('tfl.init', () => {
-        // The code you place here will be executed every time your command is executed
         run("init", null);
     });
 
     let tflSchedule = vscode.commands.registerCommand('tfl.schedule', () => {
-        // The code you place here will be executed every time your command is executed
         run("default", "internal");
     });
+
+    function getTerminal(){
+        if (terminalStack) {
+            if (terminalStack.length === 0) {
+                terminalStack.push(vscode.window.createTerminal(`tfl terminal #${terminalStack.length + 1}`));
+            }
+        } else {
+            let terminalStack = [];
+        }
+        return terminalStack[terminalStack.length - 1];
+    }
 
     function run(mode, schedule) {
         var editor = vscode.window.activeTextEditor;
@@ -42,26 +50,17 @@ export function activate(context: vscode.ExtensionContext) {
             return;
         }
 
-        if (terminalStack) {
-            if (terminalStack.length === 0) {
-                terminalStack.push(vscode.window.createTerminal(`tfl terminal #${terminalStack.length + 1}`));
-            }
-        } else {
-            let terminalStack = [];
-        }
-
         var file = editor.document.fileName;
         var folder = file.substring(0, file.lastIndexOf("\\") + 1);
-        var terminal = terminalStack[terminalStack.length - 1];
         var cfg = vscode.workspace.getConfiguration('tfl');
 
+        var terminal = getTerminal();
         terminal.show();
         terminal.sendText(`cd "${folder}"`);
-        terminal.sendText(`cls`);
-        terminal.sendText(`cls`);
+        terminal.sendText('cls');       
         
         var tfl = "tfl.exe";
-        if (cfg.path !== undefined && cfg.path !== "") {
+        if (cfg && cfg.path !== undefined && cfg.path !== "") {
             if (cfg.path[cfg.path.length - 1] !== "\\") {
                 tfl = cfg.path + "\\" + tfl;
             } else {
@@ -80,6 +79,7 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(tflSchema);
     context.subscriptions.push(tflInit);
     context.subscriptions.push(tflSchedule);
+
 }
 
 // this method is called when your extension is deactivated
